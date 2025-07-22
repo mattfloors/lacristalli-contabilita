2f833609f52879422103ea028326c8b9970d39d3
Matteo Pinto <matteliberty@yahoo.it>
2025-07-22 18:25:58 +0200

    fix

--- src/AccountingsPage.vue
+++ src/AccountingsPage.vue
@@ -10,12 +10,15 @@ import Button from "primevue/button";
 import Dialog from "primevue/dialog";
 import InputNumber from "primevue/inputnumber";
 import FileUpload from "primevue/fileupload";
+import Textarea from "primevue/textarea";
 
 const accountings = ref(data.data.data);
 
 const statuses = [
-  { name: "In corso", code: "pending" },
-  { name: "Completato", code: "completed" },
+  { name: "In attesa", code: "pending" }, // yellow
+  { name: "Incassato", code: "collected" }, // blue
+  { name: "Pagato", code: "paid" }, // red
+  { name: "Incassato e pagato", code: "collected_and_paid" }, // green
 ];
 
 const filters = ref({
@@ -89,33 +92,6 @@ const uploadFile = () => {
 
 <template>
   <div class="flex flex-col gap-10">
-    <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-2">
-      <div
-        class="bg-gray-100 p-4 rounded-lg h-28 flex flex-col justify-between"
-      >
-        <p class="text-4xl font-bold">123.00€</p>
-        <p class="text-sm">Venduto</p>
-      </div>
-      <div
-        class="bg-gray-100 p-4 rounded-lg h-28 flex flex-col justify-between"
-      >
-        <p class="text-4xl font-bold">123.00€</p>
-        <p class="text-sm">Provvigioni</p>
-      </div>
-      <div
-        class="bg-gray-100 p-4 rounded-lg h-28 flex flex-col justify-between"
-      >
-        <p class="text-4xl font-bold">12</p>
-        <p class="text-sm">In corso</p>
-      </div>
-      <div
-        class="bg-gray-100 p-4 rounded-lg h-28 flex flex-col justify-between"
-      >
-        <p class="text-4xl font-bold">12</p>
-        <p class="text-sm">Completati</p>
-      </div>
-    </div>
-
     <div class="flex gap-2">
       <InputText type="text" v-model="filters.search" placeholder="Cerca" />
 
@@ -137,6 +113,33 @@ const uploadFile = () => {
       />
     </div>
 
+    <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-2">
+      <div
+        class="bg-gray-100 p-4 rounded-lg h-28 flex flex-col justify-between"
+      >
+        <p class="text-4xl font-bold">123.00€</p>
+        <p class="text-sm">Incassi mancanti</p>
+      </div>
+      <div
+        class="bg-gray-100 p-4 rounded-lg h-28 flex flex-col justify-between"
+      >
+        <p class="text-4xl font-bold">11</p>
+        <p class="text-sm">Assicuratori di cui manca l'incasso</p>
+      </div>
+      <div
+        class="bg-gray-100 p-4 rounded-lg h-28 flex flex-col justify-between"
+      >
+        <p class="text-4xl font-bold">123.00€</p>
+        <p class="text-sm">Provvigioni da pagare</p>
+      </div>
+      <div
+        class="bg-gray-100 p-4 rounded-lg h-28 flex flex-col justify-between"
+      >
+        <p class="text-4xl font-bold">12</p>
+        <p class="text-sm">Assicuratori da pagare</p>
+      </div>
+    </div>
+
     <div>
       <DataTable :value="accountingsFiltered" tableStyle="min-width: 50rem">
         <Column header="Agente">
@@ -146,49 +149,48 @@ const uploadFile = () => {
         </Column>
         <Column header="Stato">
           <template #body="slotProps">
-            <div
+            <p
               :class="{
-                'px-2 py-1 rounded-full text-sm text-center w-32': true,
-                'bg-green-500 text-white':
-                  slotProps.data.status === 'completed',
-                'bg-yellow-500 text-white': slotProps.data.status === 'pending',
+                'px-2 py-1 rounded-md text-center': true,
+                'bg-yellow-100 text-yellow-600':
+                  slotProps.data.status === 'pending',
+                'bg-blue-100 text-blue-600':
+                  slotProps.data.status === 'collected',
+                'bg-red-100 text-red-600': slotProps.data.status === 'paid',
+                'bg-green-100 text-green-600':
+                  slotProps.data.status === 'collected_and_paid',
               }"
+              >{{
+                statuses.find((status) => status.code === slotProps.data.status)
+                  ?.name ?? "-"
+              }}</p
             >
-              <p>{{
-                slotProps.data.status === "completed"
-                  ? "Completato"
-                  : "In corso"
-              }}</p>
-            </div>
           </template>
         </Column>
-        <Column field="sold_price" header="Venduto">
+        <Column field="sold_price" header="Incassi">
           <template #body="slotProps">
-            <p>{{ priceFormatted(slotProps.data.sold_price) }}</p>
+            <p class="text-lg font-bold">
+              {{ priceFormatted(slotProps.data.sold_price) }}
+            </p>
+            <p class="text-xs text-gray-400">{{ "07/07/2025" }}</p>
+            <p class="text-xs text-gray-400">{{ "N123321 - 07/07/2025" }}</p>
           </template>
         </Column>
         <Column field="revenue" header="Provvigione">
           <template #body="slotProps">
-            <p>{{ priceFormatted(slotProps.data.revenue) }}</p>
-          </template>
-        </Column>
-        <Column field="revenue_indirect" header="Provvigione dai prospect">
-          <template #body="slotProps">
-            <p>{{ priceFormatted(slotProps.data.revenue_indirect) }}</p>
-          </template>
-        </Column>
-        <Column field="invoiced_at" header="Fatturato il">
-          <template #body="slotProps">
-            <p>{{
-              slotProps.data.invoiced_at?.toLocaleDateString("it-IT") ?? "-"
+            <p class="text-lg font-bold">
+              {{ priceFormatted(slotProps.data.revenue) }}
+            </p>
+            <p class="text-sm text-gray-400">{{
+              priceFormatted(slotProps.data.revenue)
             }}</p>
+            <p class="text-xs text-gray-400">{{ "07/07/2025" }}</p>
+            <p class="text-xs text-gray-400">{{ "N123321 - 07/07/2025" }}</p>
           </template>
         </Column>
-        <Column field="paid_at" header="Pagato il">
+        <Column field="note" header="Note">
           <template #body="slotProps">
-            <p>{{
-              slotProps.data.paid_at?.toLocaleDateString("it-IT") ?? "-"
-            }}</p>
+            <p class="text-xs">Note</p>
           </template>
         </Column>
         <Column header="Azioni">
@@ -205,17 +207,6 @@ const uploadFile = () => {
                 aria-label="Modifica"
                 severity="success"
               />
-              <Button
-                @click="
-                  () => {
-                    modelToFile = slotProps.data;
-                    fileModal = true;
-                  }
-                "
-                icon="pi pi-file"
-                aria-label="File"
-                severity="info"
-              />
             </div>
           </template>
         </Column>
@@ -225,118 +216,147 @@ const uploadFile = () => {
     <Dialog
       v-model:visible="editModal"
       modal
-      header="Modifica"
+      :header="`${modelToEdit?.insurer?.full_name ?? ''}`"
       :style="{ width: '45rem' }"
     >
-      <div class="grid sm:grid-cols-2 gap-2 py-2">
-        <div>
-          <label for="insurer" class="font-bold block mb-2">Stato</label>
-          <Dropdown
-            v-model="modelToEdit.status"
-            :options="statuses"
-            optionLabel="name"
-            optionValue="code"
-            placeholder="Scegli lo stato"
-            class="w-full"
-          />
+      <div class="flex flex-col gap-10">
+        <div class="flex flex-col gap-2">
+          <h1 class="font-bold w-full bg-red-100 px-2 py-1 rounded-md">
+            Provvigioni
+          </h1>
+          <div class="grid grid-cols-2 gap-4 mt-3">
+            <div>
+              <label for="invoiced_at" class="block mb-2"
+                >Importo imponibile fattura</label
+              >
+              <InputNumber
+                disabled
+                class="w-full"
+                :min="0"
+                v-model="modelToEdit.revenue"
+              />
+            </div>
+            <div>
+              <label for="invoiced_at" class="block mb-2">Importo pagato</label>
+              <InputNumber
+                class="w-full"
+                v-model="modelToEdit.paid_amount"
+                :min="0"
+              />
+            </div>
+            <div>
+              <label for="invoiced_at" class="block mb-2">Numero fattura</label>
+              <InputText class="w-full" v-model="modelToEdit.invoice_number" />
+            </div>
+            <div>
+              <label for="invoiced_at" class="block mb-2">Data fattura</label>
+              <Calendar
+                v-model="modelToEdit.invoiced_at"
+                dateFormat="dd/mm/yy"
+                placeholder="Scegli la data"
+                class="w-full"
+              />
+            </div>
+            <div>
+              <label for="invoiced_at" class="block mb-2"
+                >Conto di pagamento fattura</label
+              >
+              <Dropdown
+                v-model="modelToEdit.payment_account"
+                :options="['FIDEURAM', 'MPS', 'FINECO']"
+                placeholder="Scegli uno"
+                class="w-full"
+                showClear
+              />
+            </div>
+          </div>
         </div>
-        <div>
-          <label for="sold_price" class="font-bold block mb-2">Venduto</label>
-          <InputNumber
-            v-model="modelToEdit.sold_price"
-            :min="0"
-            :maxFractionDigits="2"
-            inputId="sold_price"
-            mode="currency"
-            currency="EUR"
-            class="w-full"
-          />
+        <div class="flex flex-col gap-2">
+          <h1 class="font-bold w-full bg-green-100 px-2 py-1 rounded-md">
+            Incassi
+          </h1>
+          <div class="grid grid-cols-2 gap-4 mt-3">
+            <div>
+              <label for="invoiced_at" class="block mb-2"
+                >Importo lordo incasso dovuto</label
+              >
+              <InputNumber
+                disabled
+                class="w-full"
+                :min="0"
+                v-model="modelToEdit.sold_price"
+              />
+            </div>
+            <div>
+              <label for="invoiced_at" class="block mb-2"
+                >Data ricezione incasso</label
+              >
+              <Calendar
+                v-model="modelToEdit.collected_at"
+                dateFormat="dd/mm/yy"
+                placeholder="Scegli la data"
+                class="w-full"
+              />
+            </div>
+            <div>
+              <label for="invoiced_at" class="block mb-2"
+                >Conto di pagamento fattura</label
+              >
+              <Dropdown
+                v-model="modelToEdit.payment_account_collected"
+                :options="['FIDEURAM', 'MPS', 'FINECO']"
+                placeholder="Scegli uno"
+                class="w-full"
+                showClear
+              />
+            </div>
+          </div>
         </div>
-        <div>
-          <label for="revenue" class="font-bold block mb-2">Provvigione</label>
-          <InputNumber
-            v-model="modelToEdit.revenue"
-            :min="0"
-            :maxFractionDigits="2"
-            inputId="revenue"
-            mode="currency"
-            currency="EUR"
-            class="w-full"
-          />
+        <div class="flex flex-col gap-2">
+          <h1 class="font-bold w-full bg-slate-200 px-2 py-1 rounded-md">
+            Note
+          </h1>
+          <div class="flex flex-col gap-2 mt-2">
+            <Textarea
+              v-model="modelToEdit.note"
+              autoResize
+              rows="5"
+              cols="30"
+            />
+          </div>
         </div>
-        <div>
-          <label for="revenue_indirect" class="font-bold block mb-2">
-            Provvigione dai prospect
-          </label>
-          <InputNumber
-            v-model="modelToEdit.revenue_indirect"
-            :min="0"
-            :maxFractionDigits="2"
-            inputId="revenue_indirect"
-            mode="currency"
-            currency="EUR"
-            class="w-full"
-          />
+        <div class="flex flex-col gap-2">
+          <h1 class="font-bold w-full bg-slate-200 px-2 py-1 rounded-md">
+            File presenti
+          </h1>
+          <div class="flex flex-col gap-2 mt-2">
+            <div v-for="file in modelToEdit.docs" :key="file.id">
+              <a :href="file.url" target="_blank">
+                <i class="pi pi-file mr-2" style="font-size: 1rem"></i>
+                <span>{{ file.file_name ?? "Nome file non disponibile" }}</span>
+              </a>
+            </div>
+          </div>
         </div>
-        <div>
-          <label for="invoiced_at" class="font-bold block mb-2"
-            >Fatturato il
-          </label>
-          <Calendar
-            v-model="modelToEdit.invoiced_at"
-            dateFormat="dd/mm/yy"
-            placeholder="Scegli la data"
-            class="w-full"
-          />
-        </div>
-        <div>
-          <label for="paid_at" class="font-bold block mb-2">Pagato il</label>
-          <Calendar
-            v-model="modelToEdit.paid_at"
-            dateFormat="dd/mm/yy"
-            placeholder="Scegli la data"
-            class="w-full"
-          />
+        <div class="flex flex-col gap-2">
+          <h1 class="font-bold w-full bg-slate-200 px-2 py-1 rounded-md">
+            Carica file
+          </h1>
+          <div class="flex flex-col gap-2 mt-2">
+            <FileUpload
+              v-model="filesToUpload"
+              mode="basic"
+              name="filesToUpload"
+              :multiple="true"
+              accept="application/pdf"
+              @upload="uploadFile"
+            />
+          </div>
         </div>
       </div>
       <template #footer>
         <Button label="Annulla" severity="outline" @click="editModal = false" />
         <Button label="Salva" severity="info" @click="save" />
-      </template>
-    </Dialog>
-
-    <Dialog
-      v-model:visible="fileModal"
-      modal
-      header="Gestisci i file"
-      :style="{ width: '45rem' }"
-    >
-      <div>
-        <p class="font-bold">File presenti</p>
-        <div class="flex flex-col gap-2 py-2">
-          <div v-for="file in modelToFile.docs" :key="file.id">
-            <a :href="file.url" target="_blank">
-              <i class="pi pi-file mr-2" style="font-size: 1rem"></i>
-              <span>{{ file.file_name ?? "Nome file non disponibile" }}</span>
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div class="mt-8">
-        <p class="font-bold">Carica file</p>
-        <FileUpload
-          v-model="filesToUpload"
-          mode="basic"
-          name="filesToUpload"
-          :multiple="true"
-          accept="application/pdf"
-          @upload="uploadFile"
-        />
-      </div>
-      <template #footer>
-        <Button label="Annulla" severity="outline" @click="fileModal = false" />
-        <Button label="Carica" severity="info" @click="uploadFile" />
       </template>
     </Dialog>
   </div>
